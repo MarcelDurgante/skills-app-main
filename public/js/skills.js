fb4895819f89ba04385c64965c31638c85706885
MarcelDurgante <marceldur@gmail.com>
2023-10-30 19:03:47 +0100

    Found the performance issue. Now we'll move the unecessary code outside to check memory issues

--- public/js/skills.js
+++ public/js/skills.js
@@ -1,140 +1,133 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    // Handle logout
-    const logoutButton = document.getElementById("logout");
-    logoutButton.addEventListener("click", () => logout(currentUser));
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  // Handle logout
+  const logoutButton = document.getElementById("logout");
+  logoutButton.addEventListener("click", () => logout(currentUser));
 
-    checkLoggedInUser();
-    handlePage(currentUser);
-    updateNavbar();
+  checkLoggedInUser();
+  handlePage(currentUser);
+  updateNavbar();
 });
 
 function checkLoggedInUser() {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-    if (!currentUser) {
-        // If no user is logged in, redirect to the login page
-        window.location.href = "main.html";
-        return;
-    }
+  if (!currentUser) {
+    // If no user is logged in, redirect to the login page
+    window.location.href = "main.html";
+    return;
+  }
 }
 
 function logout(currentUser) {
-    fetch("/api/user/logout", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
+  fetch("/api/user/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        localStorage.removeItem("currentUser");
+        window.location.href = "main.html";
+      } else {
+        console.error("Error logging out:", data.message);
+      }
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === "success") {
-                localStorage.removeItem("currentUser");
-                window.location.href = "main.html";
-            } else {
-                console.error("Error logging out:", data.message);
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function handlePage(currentUser) {
-    // Handle skill list page
-    if (window.location.pathname.endsWith("skillList.html")) {
-        renderSkillListPage();
-    }
+  // Handle skill list page
+  if (window.location.pathname.endsWith("skillList.html")) {
+    renderSkillListPage();
+  }
 
-    // Handle add skill page
-    if (window.location.pathname.endsWith("skillForm.html")) {
+  // Handle add skill page
+  if (window.location.pathname.endsWith("skillForm.html")) {
+    const addSkillForm = document.getElementById("add-skill-form");
 
-        const addSkillForm = document.getElementById("add-skill-form");
-
-        addSkillForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            addSkill(currentUser);
-        });
-    }
+    addSkillForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      addSkill(currentUser);
+    });
+  }
 }
 
 function addSkill() {
-
-    const messageDiv = document.getElementById("message");
-    const skillName = document.getElementById("skill-name").value;
-    fetch("/api/user/skills", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ skill: skillName }),
-        credentials: 'include'
+  const messageDiv = document.getElementById("message");
+  const skillName = document.getElementById("skill-name").value;
+  fetch("/api/user/skills", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ skill: skillName }),
+    credentials: "include",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        // if HTTP-status is 200-299
+        // get the error message from the body or default to response statusText
+        throw new Error(response.statusText);
+      }
+      return response.json();
     })
-        .then(response => {
-            if (!response.ok) { // if HTTP-status is 200-299
-                // get the error message from the body or default to response statusText
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.status === "success") {
-                messageDiv.textContent = "Skill added successfully!";
-            } else {
-                messageDiv.textContent = data.message;
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+    .then((data) => {
+      if (data.status === "success") {
+        messageDiv.textContent = "Skill added successfully!";
+      } else {
+        messageDiv.textContent = data.message;
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function renderSkillListPage() {
-    const skillsList = document.getElementById("skills-list");
+  const skillsList = document.getElementById("skills-list");
 
-    // Get username from local storage for log message
-    let user = JSON.parse(localStorage.getItem('currentUser'));
-    console.log("Getting skills for " + user.username);
+  // Get username from local storage for log message
+  let user = JSON.parse(localStorage.getItem("currentUser"));
+  console.log("Getting skills for " + user.username);
 
-    fetch(`/api/user/skills`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === "success") {
-                data.skills.forEach(skill => {
-                    // useless intentional addition to create terrible performance so we can inspect code using the Performance tab 
-                    let list =[];
-                    for(let i = 0; i < 10000000; i++) {
-                        list[i] = Math.sqrt(i);
-                    }
-                    // end of useless addition             
-                    const listItem = document.createElement("li");
-                    listItem.textContent = skill;
-                    skillsList.appendChild(listItem);
-                });
-            } else {
-                console.error("Error getting skills:", data.message);
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
+  fetch(`/api/user/skills`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        data.skills.forEach((skill) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = skill;
+          skillsList.appendChild(listItem);
         });
+      } else {
+        console.error("Error getting skills:", data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 function updateNavbar() {
-    const user = JSON.parse(localStorage.getItem('currentUser'));
-    const navbar = document.getElementById('navbar');
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const navbar = document.getElementById("navbar");
 
-    if (user && user.isAdmin) {
-        const adminLink = document.createElement('a');
-        adminLink.href = 'admin.html'; // Set the correct link to your admin page
-        adminLink.textContent = 'Admin Page';
-        navbar.appendChild(adminLink);
-    }
+  if (user && user.isAdmin) {
+    const adminLink = document.createElement("a");
+    adminLink.href = "admin.html"; // Set the correct link to your admin page
+    adminLink.textContent = "Admin Page";
+    navbar.appendChild(adminLink);
+  }
 }
