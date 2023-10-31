8a0d534d397d93c3a6e944830267dfd442599b3f
MarcelDurgante <marceldur@gmail.com>
2023-10-31 23:30:23 +0100

    Cleaned up comments

--- public/js/skills.js
+++ public/js/skills.js
@@ -110,10 +110,9 @@ function renderSkillListPage() {
           const listItem = document.createElement("li");
           listItem.textContent = skill;
           listItem.id = skill;
-          // adding a delete button but "forgetting" event handler
           const deleteBtn = document.createElement("button");
           deleteBtn.innerHTML = "Delete";
-          deleteBtn.addEventListener("click", deleteSkill); // adding the event handler that was missing now the delete button works in the frontend (see explanation of this debugging in comment below - comments deleted after finishing the demo so look at previous commits)
+          deleteBtn.addEventListener("click", deleteSkill); 
           listItem.appendChild(deleteBtn);
           skillsList.appendChild(listItem);
         });
@@ -142,27 +141,3 @@ function deleteSkill(e) {
     document.getElementById(idToDelete).remove();
     // NOTE: not deleting it on the backend yet 
 }
-
-// Summary of the debugging Undefined behaviour
-
-/* output: 
-
-This time, we start in the Elements panel because we want to see if any event listeners are attached. We have the Sources panel as a backup plan, and we end up fixing our code. 
-
-We try to add a Delete button to every skill. Let's log in as user1 again. And as you can see, every skill now has a Delete button. But when I click it, nothing is happening. So the first step would be to check out the Elements tab. 
-
-I'm going to select one of the buttons, and I'm going to navigate to the event listeners tab. And as you can see, nothing seems to be connected here. So that's already a really good clue that this is not working. 
-
-Sometimes, however, it can be a bit vague to see what's really going on. And now we still have the Sources tab as a backup. So in here, at the very bottom, we have a deleteSkill function. So I'm going put a breakpoint on here, click those buttons, and as I can see, nothing is happening. So that should be a clue. We should have a look at where I added that event handler. 
-
-Where did we add that event handler? Well, it turns out we didn't. 
-
-So, let's make sure that we add the event handler to the click so that now the Delete button is actually connected to the deleteSkill function. 
-
-And then now when we refresh, you can see that we can actually end up in this function. And if you step through it (using the step over button of the inspector debugger of the DevsTool), the elements get deleted. 
-
-So let me repeat without a breakpoint. When I click on this, you can see it gets deleted.
-
-So what's not implemented yet is that it's also deleted on the back end. So when I refresh it again, all my skills are back. 
-
-*/
