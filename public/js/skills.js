6c14f9e5fdbd2ad83e34c87a51b0167d5cb434c7
MarcelDurgante <marceldur@gmail.com>
2023-11-03 02:28:37 +0100

    Errors fixed

--- public/js/skills.js
+++ public/js/skills.js
@@ -67,7 +67,7 @@ async function addSkill() {
   const skillName = document.getElementById("skill-name").value;
 
   try {
-    const response = fetch("/api/user/skills", {
+    const response = await fetch("/api/user/skills", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -83,7 +83,7 @@ async function addSkill() {
       throw new Error(response.statusText);
     }
 
-    const data = response.json(); // Bug: response is still a promise
+    const data = await response.json(); // Bug: response is still a promise
     if (data.status === "success") {
       messageDiv.textContent = "Skill added successfully!";
     } else {
@@ -146,3 +146,19 @@ function deleteSkill(e) {
   document.getElementById(idToDelete).remove();
   // NOTE: not deleting it on the backend yet
 }
+
+/* DESCRIPTION: Solution summary: 
+
+Problem when we try to add a skill. You can see we get a console error. It says there was an error with adding the skill, and it happened at skills.js at line 79. 
+
+So let's go ahead and have a look here in our skills.js at line 79. This is where it shows our error. But most commonly, you do not have to comment with what the exact bug is above. So let's go ahead and debug this. 
+
+Let's set a breakpoint at the line where I'm doing the fetch, and there's something that I'm noticing and that's that I forgot to use the await keyword here. So, as a result, our response will be a promise object and not the actual response object. 
+
+So when we try to access the response.ok on line 77, we're attempting to access the properties on a promise object and not on the actual response object itself. And that's going to lead to bugs. 
+
+So the data variable will also be a promise and not the actual data object. So checking the data status and accessing data message will not work as expected. 
+
+So let me show you what happens. I'm going to click Add Skill again. And you can see I end up right here. You can see the response is now undefined. But when I step to the next line, you can see it's a promise object, and it's still pending. The promise object does not have the okay, and that's why we end up in our error. So how to fix this? I need to use the await keyword when I'm calling the fetch, and I need to use the await keyword for the response.json because response.json is also going to return a promise and not the actual object. This ensures that we wait for the async operations to complete before proceeding. So now when we've added these changes and I go back, I'm going to remove my breakpoints and then reload the page again. Now if I try to add a skill, you can see that this skill was added successfully and that there are no console errors.
+
+*/
